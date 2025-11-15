319e435881d04c9bcd53bfe748e937193e2c216f
nyamulacoretta-ops <nyamulacoretta@gmail.com>
2025-11-15 12:05:02 +0200

    added pydantic models

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from typing import Optional
+
 import fastapi
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = False
+    rating: Optional[float] = None
+
 #path operation or route
 @app.get("/")  #this decorator makes the function to api. get is http method
 #It is function so we add async because we will do asynchronous coding
@@ -14,7 +25,11 @@ def get_posts():
     return {"data": "This is your post"}
 
 @app.post("/createpost")
-def create_post(name: dict = Body(...)):
-    print(name)
-    return {"new_post": f"title {name['title']} content {name['content']}"}
-    return {"message": "Successfully created post"}
+def create_post(new_post: Post):
+    print(new_post.rating)
+    return {"data": "This is your post"}
+
+    # you can also translate a pydantic model into a dictionary
+    # print(new_post.dict()) to list all the dictionaries
+
+
