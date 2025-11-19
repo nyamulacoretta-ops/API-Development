f16a508feca3aa6eba7694c134f5d2668bad2145
nyamulacoretta-ops <nyamulacoretta@gmail.com>
2025-11-19 20:24:19 +0200

    Started creating apis for our CRUD application. We only dit post and get http request. We also added and updated HHTP status codes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from random import randrange
 from typing import Optional
 
 import fastapi
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Response, HTTPException,status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,6 +14,12 @@ class Post(BaseModel):
     published: bool = False
     rating: Optional[float] = None
 
+#array
+my_posts = [
+    {"title": "post title", "content": "post content","id": 1},
+    {"title": "My favourate colour", "content": "Color content", "id": 2 },
+]
+
 #path operation or route
 @app.get("/")  #this decorator makes the function to api. get is http method
 #It is function so we add async because we will do asynchronous coding
@@ -22,14 +29,39 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
-@app.post("/createpost")
-def create_post(new_post: Post):
-    print(new_post.rating)
-    return {"data": "This is your post"}
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 100000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
     # you can also translate a pydantic model into a dictionary
     # print(new_post.dict()) to list all the dictionaries
+def find_post(id: int) -> Optional[Post]:
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+    return None
+
+@app.post("/posts/1")
+def latest_post(post: Post):
+    var = my_posts[len(my_posts) - 1]
+    return {"data": var}
+
+    #@app.get("/posts/{id}")
+#def get_post(id: int):
+    print(id)
+    return {"data": "{id}".format(id=id)}
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post with {id} not found")
+    return {"data": post}
+#to get latest post
+
 
 
